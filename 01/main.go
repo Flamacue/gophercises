e8979fc0a9d9152be0783e34a9e1ddc86fc82522
Adam Sumner <asumner101@gmail.com>
2019-04-11 19:56:27 -0400

    add quiz game

---
+++ 01/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+// TODO(asumner): handle errors in a better manner than panic
+
+// A config holds the configuration values for the quiz game
+type config struct {
+	timeLimit        int    // maximum time limit for the quiz game
+	problemsFilePath string // path to the file containing the quiz questions
+}
+
+type problem struct {
+	problem string
+	answer  string
+}
+
+// Constants
+const (
+	timeLimitDefault        = 30                  // default time limit
+	problemsFilePathDefault = "/etc/problems.csv" // default path to problems file
+)
+
+const welcomeText = `Welcome to the Quiz Show!
+
+The quiz will have a time limit of %d seconds
+
+We will be presenting problems located from %s
+
+Do you accept this challenge? [y/N]
+`
+
+// Parses cmd line flags and returns a config
+func buildConfiguration() *config {
+	var timeLimit int
+	var problemsFilePath string
+
+	flag.IntVar(&timeLimit, "time", timeLimitDefault, "Time limit for the quiz game in seconds")
+	flag.IntVar(&timeLimit, "t", timeLimitDefault, "Time limit for the quiz game in seconds")
+	flag.StringVar(&problemsFilePath, "file", problemsFilePathDefault, "Time limit for the quiz game in seconds")
+	flag.StringVar(&problemsFilePath, "f", problemsFilePathDefault, "Time limit for the quiz game in seconds")
+	flag.Parse()
+
+	return &config{timeLimit, problemsFilePath}
+}
+
+// Main entry point to our program
+// Executes the main logic loop
+func main() {
+	conf := buildConfiguration()
+	fmt.Printf(welcomeText, conf.timeLimit, conf.problemsFilePath)
+	var accept string
+	_, err := fmt.Scanf("%s", &accept)
+	if err != nil {
+		panic(err)
+	}
+
+	if strings.ToUpper(accept) != "Y" {
+		fmt.Println("Not so brave now are you?")
+		os.Exit(0)
+	}
+
+	fmt.Println() // Newline for formatting
+	problems := parseProblems(conf.problemsFilePath)
+	beginQuiz(problems, conf.timeLimit)
+	os.Exit(0)
+}
+
+func beginQuiz(problems []*problem, timeLimit int) {
+	totalProblems := len(problems)
+	correct := 0
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	for i, problem := range problems {
+		fmt.Printf("Problem %d: %s\n", i, problem.problem)
+
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			_, err := fmt.Scanf("%s", &answer)
+			if err != nil {
+				panic(err)
+			}
+			answerChan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("Times up! Thanks for playing! You got %d correct out of %d total problems\n", correct, totalProblems)
+			return
+		case answer := <-answerChan:
+			if answer == problem.answer {
+				correct++
+				fmt.Println("Correct!")
+			} else {
+				fmt.Println("Incorrect!")
+			}
+		}
+	}
+
+	fmt.Printf("Total Problems: %d, Number correct: %d\n", totalProblems, correct)
+
+}
+
+// This is a naive file read function that will load an entire csv file's contents
+// into memory and return a slice of problems
+func parseProblems(filePath string) []*problem {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var problems []*problem
+	for _, line := range lines {
+		problems = append(problems, &problem{line[0], strings.TrimSpace(line[1])})
+	}
+
+	return problems
+}
